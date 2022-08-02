a50b30b03418e923c549296222b4e450844741a3
Asgar9 <asgaryas@code.edu.az>
2022-08-02 17:11:47 +0400

    bounen

---
+++ 7ebolunen/7ebolunen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _7ebolunen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 23, m = 125,count=0;
+            for (int i = n; i < m; i++)
+            {
+                if (i%7==0)
+                {
+                    count++;
+                    Console.WriteLine( count);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
